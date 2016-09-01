6e8361b80cf8ce1f277cbcf48d9f9ba8632ed08a
Vladislavs Marisevs <vxm93@eis.equifax.com>
2016-09-01 13:55:47 +0100

    application arguments

--- spring-boot-journal/src/main/java/com/apress/spring/SpringBootJournalApplication.java
+++ spring-boot-journal/src/main/java/com/apress/spring/SpringBootJournalApplication.java
@@ -1,13 +1,23 @@
 package com.apress.spring;
 
 import java.io.PrintStream;
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
 import com.apress.spring.domain.Journal;
 import com.apress.spring.repository.JournalRepository;
@@ -23,33 +33,42 @@ public class SpringBootJournalApplication {
         repo.save(new Journal("Spring Boot Reading","Read more about Spring Boot","02/01/2016"));
         repo.save(new Journal("Spring Boot in the Cloud","Spring Boot using Cloud Foundry","03/01/2016"));
       };
-   }
+   }	
 	
 	
 	public static void main(String[] args) {
-		// SpringApplication.run(SpringBootJournalApplication.class, args);
-		
-		SpringApplication app = new SpringApplication(SpringBootJournalApplication.class);
-		
-		// http://patorjk.com/
-//		app.setBanner(new Banner() {
-//			
-//			@Override
-//			public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
-//				// TODO Auto-generated method stub
-//				out.println("   ____  ____             _               ____              _    ____  ");
-//				out.println("  / / / / ___| _ __  _ __(_)_ __   __ _  | __ )  ___   ___ | |_  \\ \\ \\ ");
-//				out.println(" / / /  \\___ \\| '_ \\| '__| | '_ \\ / _` | |  _ \\ / _ \\ / _ \\| __|  \\ \\ \\");
-//				out.println(" \\ \\ \\   ___) | |_) | |  | | | | | (_| | | |_) | (_) | (_) | |_   / / /");
-//				out.println("  \\_\\_\\ |____/| .__/|_|  |_|_| |_|\\__, | |____/ \\___/ \\___/ \\__| /_/_/ ");
-//				out.println("              |_|                 |___/                                ");
-//				out.println("");
-//		          
-//			}
-//		});
+
 		
 		
+		new SpringApplicationBuilder(SpringBootJournalApplication.class)
+	        // .bannerMode(Banner.Mode.OFF)
+	        //.sources(SpringBootJournalApplication.class)
+			//.logStartupInfo(false)		
+			.web(false)
+	        .run(args);
+	}
+	
+	
+	
+	
+	
+	
+	@Component
+	static
+	class MyComponent{
 		
-		app.run(args);
+		private static final Logger log = LoggerFactory.getLogger(MyComponent.class); 
+
+	     @Autowired
+	     public MyComponent(ApplicationArguments args) {
+	            boolean enable = args.containsOption("enable");
+	            if(enable)
+	                  log.info("## > You are enable!");
+
+	           List<String> _args = args.getNonOptionArgs();
+	                  log.info("## > extra args ...");
+	           if(!_args.isEmpty())
+	                 _args.forEach(file -> log.info(file));
+	     }     
 	}
 }
