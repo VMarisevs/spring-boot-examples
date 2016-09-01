22e9ec44c2c23bbfe5f5b66c95f4b782a620e5ef
Vladislavs Marisevs <vxm93@eis.equifax.com>
2016-09-01 13:36:43 +0100

    changing spring boot banner

--- spring-boot-journal/src/main/java/com/apress/spring/SpringBootJournalApplication.java
+++ spring-boot-journal/src/main/java/com/apress/spring/SpringBootJournalApplication.java
@@ -1,9 +1,13 @@
 package com.apress.spring;
 
+import java.io.PrintStream;
+
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 import com.apress.spring.domain.Journal;
 import com.apress.spring.repository.JournalRepository;
@@ -23,6 +27,29 @@ public class SpringBootJournalApplication {
 	
 	
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootJournalApplication.class, args);
+		// SpringApplication.run(SpringBootJournalApplication.class, args);
+		
+		SpringApplication app = new SpringApplication(SpringBootJournalApplication.class);
+		
+		// http://patorjk.com/
+//		app.setBanner(new Banner() {
+//			
+//			@Override
+//			public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+//				// TODO Auto-generated method stub
+//				out.println("   ____  ____             _               ____              _    ____  ");
+//				out.println("  / / / / ___| _ __  _ __(_)_ __   __ _  | __ )  ___   ___ | |_  \\ \\ \\ ");
+//				out.println(" / / /  \\___ \\| '_ \\| '__| | '_ \\ / _` | |  _ \\ / _ \\ / _ \\| __|  \\ \\ \\");
+//				out.println(" \\ \\ \\   ___) | |_) | |  | | | | | (_| | | |_) | (_) | (_) | |_   / / /");
+//				out.println("  \\_\\_\\ |____/| .__/|_|  |_|_| |_|\\__, | |____/ \\___/ \\___/ \\__| /_/_/ ");
+//				out.println("              |_|                 |___/                                ");
+//				out.println("");
+//		          
+//			}
+//		});
+		
+		
+		
+		app.run(args);
 	}
 }
