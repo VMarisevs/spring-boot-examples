1a670e53e93f63ad43511c2e0444d37c357cb8db
Vladislavs Marisevs <vxm93@eis.equifax.com>
2016-09-01 14:27:16 +0100

    passing arguments and .properties

--- spring-boot-journal/src/main/java/com/apress/spring/SpringBootJournalApplication.java
+++ spring-boot-journal/src/main/java/com/apress/spring/SpringBootJournalApplication.java
@@ -1,30 +1,27 @@
 package com.apress.spring;
 
-import java.io.PrintStream;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.Banner.Mode;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
-
 import com.apress.spring.domain.Journal;
 import com.apress.spring.repository.JournalRepository;
 
 @SpringBootApplication
 public class SpringBootJournalApplication {
 
+	/*
+	 * mvn spring-boot:run -Dserver.ip=192.168.12.1
+	 * 
+	 * java -jar target/spring-boot-config-0.0.1-SNAPSHOT.jar --server.ip=192.168.12.1
+	 */
+	
+	private static Logger log = LoggerFactory.getLogger(SpringBootJournalApplication.class);
+	
 	@Bean
     InitializingBean saveData(JournalRepository repo){
     return () -> {
@@ -33,46 +30,23 @@ public class SpringBootJournalApplication {
         repo.save(new Journal("Spring Boot Reading","Read more about Spring Boot","02/01/2016"));
         repo.save(new Journal("Spring Boot in the Cloud","Spring Boot using Cloud Foundry","03/01/2016"));
       };
-   }	
+   }
+	
+	@Value("${server.ip}") 
+	String serverIp; 
+
+	@Bean
+	CommandLineRunner values(){
+		return args -> {
+			log.info(" > The Server IP is: " +     serverIp );
+		};
+	} 
 	
 	
 	public static void main(String[] args) {
 
-		
-		
 		new SpringApplicationBuilder(SpringBootJournalApplication.class)
-	        // .bannerMode(Banner.Mode.OFF)
-	        //.sources(SpringBootJournalApplication.class)
-			//.logStartupInfo(false)		
-			.web(false)
 	        .run(args);
 	}
 	
-	
-	
-	
-	
-	
-	@Component
-	static class MyComponent{
-		
-		private static final Logger log = LoggerFactory.getLogger(MyComponent.class); 
-
-		/*
-		 * mvn spring-boot:run -Drun.arguments="--enable"
-		 * mvn spring-boot:run -Drun.arguments="arg1,arg2"
-		 */
-		
-	     @Autowired
-	     public MyComponent(ApplicationArguments args) {
-	            boolean enable = args.containsOption("enable");
-	            if(enable)
-	                  log.info("## > You are enable!");
-
-	           List<String> _args = args.getNonOptionArgs();
-	                  log.info("## > extra args ...");
-	           if(!_args.isEmpty())
-	                 _args.forEach(file -> log.info(file));
-	     }     
-	}
 }
