c28abad4744e825929ed62af838ca359b386eb02
Vladislavs Marisevs <vxm93@eis.equifax.com>
2016-09-02 10:30:57 +0100

    init jdbc..

--- spring-boot-testing/src/test/java/com/apress/spring/SprintBootWebApplicationTests.java
+++ spring-boot-testing/src/test/java/com/apress/spring/SprintBootWebApplicationTests.java
@@ -13,20 +13,16 @@ import java.io.IOException;
 import java.nio.charset.Charset;     
 import java.util.Arrays;
 
-import org.junit.Before; 
-import org.junit.FixMethodOrder; 
+import org.junit.Before;
 import org.junit.Test; 
 import org.junit.runner.RunWith; 
-import org.junit.runners.MethodSorters; 
 import org.springframework.beans.factory.annotation.Autowired; 
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType; 
 import org.springframework.http.converter.HttpMessageConverter; 
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.mock.http.MockHttpOutputMessage; 
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner; 
-import org.springframework.test.context.web.WebAppConfiguration; 
 import org.springframework.test.web.servlet.MockMvc; 
 import org.springframework.web.context.WebApplicationContext;
 
